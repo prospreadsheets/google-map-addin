0838e3fcac1428ff9c4a48bc0cda73d118856ad8
Rama Chandra Rao Nokavarapu <ramatheram@gmail.com>
2026-07-15 11:24:57 +0530

    fix commands files remove outlook handler

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -5,31 +5,6 @@
 
 /* global Office */
 
-Office.onReady(() => {
-  // If needed, Office.js is ready to be called.
+Office.onReady(function() {
+    // No event-based handlers needed for this add-in
 });
-
-/**
- * Shows a notification when the add-in command is executed.
- * @param event {Office.AddinCommands.Event}
- */
-function action(event) {
-  const message = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
-
-  // Show a notification message.
-  Office.context.mailbox.item.notificationMessages.replaceAsync(
-    "ActionPerformanceNotification",
-    message
-  );
-
-  // Be sure to indicate when the add-in command function is complete.
-  event.completed();
-}
-
-// Register the function with Office.
-Office.actions.associate("action", action);
